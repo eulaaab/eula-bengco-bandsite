f98dfd881835da7e2349e525363defa790fa79ba
Eula Bengco <ebengco@Eulas-MacBook-Air.local>
2020-01-30 00:25:59 -0500

    added responsive tablet and desktop, added js arrays and comment section html structure

---
+++ sprint-2/js/shows.js
@@ -0,0 +1,32 @@
+let showObj = [
+  {
+    date: "Mon Dec 17 2018",
+    venue: "Ronald Lane",
+    location: "San Fancisco, CA"
+  },
+  {
+    date: "Tue Jul 18 2019",
+    venue: "Pier 3 East",
+    location: "San Fancisco, CA"
+  },
+  {
+    date: "Fri Jul 22 2019",
+    venue: "View Loungue",
+    location: "San Fancisco, CA"
+  },
+  {
+    date: "Sat Aug 12 2019",
+    venue: "Hyatt Agency",
+    location: "San Fancisco, CA"
+  },
+  {
+    date: "Fri Sep 05 2019",
+    venue: "Moscow Center",
+    location: "San Fancisco, CA"
+  },
+  {
+    date: "Wed Aug 11 2019",
+    venue: "Pres Club",
+    location: "San Fancisco, CA"
+  }
+];
