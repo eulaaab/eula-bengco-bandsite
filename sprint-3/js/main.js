6126601082c03b1743d1834f17eafd20385e60ba
Eula Bengco <ebengco@Eulas-MacBook-Air.local>
2020-02-06 18:13:27 -0500

    added new comment posting code

--- sprint-3/js/main.js
+++ sprint-3/js/main.js
@@ -1,110 +1,91 @@
 // Eula Bengco MAIN JAVASCRIPT FILE
-// let x = [
-//   {
-//     name: "Micheal Lyons",
-//     date: "12/18/2018",
-//     text:
-//       "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
-//   },
-//   {
-//     name: "Gary Wong",
-//     date: "12/12/2018",
-//     text:
-//       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
-//   },
-//   {
-//     name: "Theodore Duncan",
-//     date: "11/15/2018",
-//     text:
-//       "How can someone be so good!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
-//   }
-// ];
 
 //grabs the parent container
-let commentParent = document.querySelector([".comment__container"]);
+let commentParent = document.querySelector(".comment__container");
 
-let commentsObj = axios
-  .get(
-    "https://project-1-api.herokuapp.com/comments?api_key=5276e2d7-0178-438b-b62f-748cd9a1f823"
-  )
-  .then(response => {
-    // console.log(response);
-    let commentsArr = response.data;
-
-    commentsArr.forEach(zData => {
-      // console.log(zData);
-      let commentWrapper = document.createElement("div");
-      commentWrapper.classList.add("comment-wrapper");
-      commentParent.appendChild(commentWrapper);
-
-      //LEFT SIDE
-      let commentDivLeft = document.createElement(["div"]);
-      commentDivLeft.classList.add("comment-left");
-      commentWrapper.appendChild(commentDivLeft);
-
-      let commentDivRight = document.createElement(["div"]);
-      commentDivRight.classList.add("comment-right");
-      commentWrapper.appendChild(commentDivRight);
-
-      let commentDivRightTop = document.createElement(["div"]);
-      commentDivRightTop.classList.add("comment-right-top");
-      commentDivRight.appendChild(commentDivRightTop);
-
-      let commentName = document.createElement("div");
-      commentName.classList.add("comment-full-name");
-      commentDivRightTop.appendChild(commentName);
-      let commentNameItem = (commentName.innerText = zData.name);
-      // console.log(commentNameItem);
-
-      let commentDate = document.createElement("div");
-      commentDate.classList.add("comment-date");
-      commentDivRightTop.appendChild(commentDate);
-      commentDate.innerText = zData.timestamp;
-      // console.log(zData.timstamp);
-
-      let commentComment = document.createElement("div");
-      commentComment.classList.add("comment-comment");
-      commentComment.innerText = zData.comment;
-      commentDivRight.appendChild(commentComment);
-      // console.log(zData.comment);
+function axiosGetFn() {
+  let axiosGet = axios
+    .get(
+      "https://project-1-api.herokuapp.com/comments?api_key=5276e2d7-0178-438b-b62f-748cd9a1f823"
+    )
+    .then(response => {
+      // console.log(response);
+      let commentsArr = response.data;
+      commentsArr.forEach(displayComment);
+      console.log(commentsArr);
     });
-    commentsDateArr = commentsArr[0].timestamp;
-    console.log(commentsDateArr);
-  });
+}
 
-//   //For the date
-//   let dateForm = new Date();
+axiosGetFn();
 
-//   let newCommentDateDay = dateForm.getDate();
-//   let newCommentDateMonth = dateForm.getMonth() + 1;
-//   let newCommentDateYear = dateForm.getFullYear();
-//   let newCommentDate = `${newCommentDateDay}/${newCommentDateMonth}/${newCommentDateYear}`;
+//commentsObjArrData
 
-//   console.log(newCommentDate);
+function displayComment(commentsObjArrData) {
+  // console.log(zData);
+  let commentWrapper = document.createElement("div");
+  commentWrapper.classList.add("comment-wrapper");
+  commentParent.appendChild(commentWrapper);
 
-//   let newDate = displayComment.target.comment.value;
-//   console.log(newDate);
-// });
+  //LEFT SIDE
+  let commentDivLeft = document.createElement("div");
+  commentDivLeft.classList.add("comment-left");
+  commentWrapper.appendChild(commentDivLeft);
 
-let form = document.getElementById(["comment__form"]);
-console.log(form);
+  let commentPhoto = document.createElement("div");
+  commentPhoto.classList.add("photo");
+  commentDivLeft.appendChild(commentPhoto);
+
+  //RIGHT SIDE
+  let commentDivRight = document.createElement("div");
+  commentDivRight.classList.add("comment-right");
+  commentWrapper.appendChild(commentDivRight);
+
+  let commentDivRightTop = document.createElement("div");
+  commentDivRightTop.classList.add("comment-right-top");
+  commentDivRight.appendChild(commentDivRightTop);
+
+  let commentName = document.createElement("div");
+  commentName.classList.add("comment-full-name");
+  commentDivRightTop.appendChild(commentName);
+  commentName.innerText = commentsObjArrData.name;
+  // console.log(commentNameItem);
+
+  let commentDate = document.createElement("div");
+  commentDate.classList.add("comment-date");
+  commentDivRightTop.appendChild(commentDate);
+
+  let formatDate = new Date(commentsObjArrData.timestamp);
+  let formatDateDay = formatDate.getDay();
+  let formatDateMonth = formatDate.getMonth() + 1;
+  let formatDateYear = formatDate.getFullYear();
+  let commentDateFormat = `${formatDateDay}/${formatDateMonth}/${formatDateYear}`;
+  commentDate.innerText = commentDateFormat;
+
+  commentsObjArrData.sort(compare());
+
+  let commentComment = document.createElement("div");
+  commentComment.classList.add("comment-comment");
+  commentComment.innerText = commentsObjArrData.comment;
+  commentDivRight.appendChild(commentComment);
+}
+
+function compare( {
+  let dateOne = new Date(one.timestamp);
+  let dateTwo = new Date(two.timstamp);
+  return dateOne - dateTwo;
+});
+
+let form = document.getElementById("comment__form");
 form.addEventListener("submit", displayComment => {
   displayComment.preventDefault();
-
   let newName = displayComment.target.name.value;
   console.log(newName);
   let newComment = displayComment.target.comment.value;
   console.log(newComment);
 
-  //date stuff
-  let dateForm = new Date();
-  let newCommentDateDay = dateForm.getDate();
-  let newCommentDateMonth = dateForm.getMonth() + 1;
-  let newCommentDateYear = dateForm.getFullYear();
-  let newCommentDate = `${newCommentDateDay}/${newCommentDateMonth}/${newCommentDateYear}`;
-  console.log(newCommentDate);
+  document.querySelector(".text").value = "";
+  document.querySelector(".text-two").value = "";
 
-  // let newCommentPost =
   axios
     .post(
       "https://project-1-api.herokuapp.com/comments?api_key=5276e2d7-0178-438b-b62f-748cd9a1f823",
@@ -113,74 +94,13 @@ form.addEventListener("submit", displayComment => {
         comment: newComment
       }
     )
+
     .then(res => {
-      axios.get(
-        "https://project-1-api.herokuapp.com/comments?api_key=5276e2d7-0178-438b-b62f-748cd9a1f823"
-      );
       console.log(res);
-      let commentWrapper = document.createElement("div");
-      commentWrapper.classList.add("comment-wrapper");
-      commentParent.appendChild(commentWrapper);
-
-      //LEFT SIDE
-      let commentDivLeft = document.createElement(["div"]);
-      commentDivLeft.classList.add("comment-left");
-      commentWrapper.appendChild(commentDivLeft);
-
-      let commentDivRight = document.createElement(["div"]);
-      commentDivRight.classList.add("comment-right");
-      commentWrapper.appendChild(commentDivRight);
-
-      let commentDivRightTop = document.createElement(["div"]);
-      commentDivRightTop.classList.add("comment-right-top");
-      commentDivRight.appendChild(commentDivRightTop);
-
-      let commentName = document.createElement("div");
-      commentName.classList.add("comment-full-name");
-      commentDivRightTop.appendChild(commentName);
-      commentName.innerText = newName;
-      // console.log(commentNameItem);
-
-      let commentDate = document.createElement("div");
-      commentDate.classList.add("comment-date");
-      commentDivRightTop.appendChild(commentDate);
-      commentDate.innerText = newCommentDate;
-      // console.log(zData.timstamp);
-
-      let commentComment = document.createElement("div");
-      commentComment.classList.add("comment-comment");
-      commentComment.innerText = newComment;
-      commentDivRight.appendChild(commentComment);
-      // console.log(zData.comment);
-    })
-    .catch(err => {
-      console.log(err);
+      axiosGetFn();
     });
+    // .catch(res => {
+    //   console.log('it errored out');
+    // });
+    //PUT A CATCH
 });
-
-// axios
-//     .get(
-//       "https://project-1-api.herokuapp.com/comments?api_key=5276e2d7-0178-438b-b62f-748cd9a1f823"
-// ,
-// {
-//   name: newName,
-//   comment: newComment
-// }
-//     )
-//   .then(res => {
-//   console.log(res);
-// })
-// .catch(err => {
-//   console.log(err);
-// });
-// console.log(response);
-
-// newCommentPost;
-// let newCommentGet = axios.post(
-//   "https://project-1-api.herokuapp.com/comments?api_key=5276e2d7-0178-438b-b62f-748cd9a1f823",
-//   {
-//     name: newName,
-//     comment: newComment,
-//     timestamp: newDate
-//   }
-// );
--- sprint-3/js/shows.js
+++ sprint-3/js/shows.js
@@ -1,56 +1,20 @@
 // Eula Bengco SHOWS JAVASCRIPT FILE
-// let showObj = [
-//   {
-//     date: "Mon Dec 17 2018",
-//     venue: "Ronald Lane",
-//     location: "San Fancisco, CA"
-//   },
-//   {
-//     date: "Tue Jul 18 2019",
-//     venue: "Pier 3 East",
-//     location: "San Fancisco, CA"
-//   },
-//   {
-//     date: "Fri Jul 22 2019",
-//     venue: "View Loungue",
-//     location: "San Fancisco, CA"
-//   },
-//   {
-//     date: "Sat Aug 12 2019",
-//     venue: "Hyatt Agency",
-//     location: "San Fancisco, CA"
-//   },
-//   {
-//     date: "Fri Sep 05 2019",
-//     venue: "Moscow Center",
-//     location: "San Fancisco, CA"
-//   },
-//   {
-//     date: "Wed Aug 11 2019",
-//     venue: "Pres Club",
-//     location: "San Fancisco, CA"
-//   }
-// ];
 
 //MAKES SHOWS HEADING FOR TABLET AND DESKTOP
-let parentContainer = document.querySelector([".shows"]);
+let parentContainer = document.querySelector(".shows");
 let showHeading = document.createElement("h2");
-//need to put this so my Shows header will get the heading 2 styling
-showHeading.classList.add("heading-2");
 showHeading.classList.add("shows__heading");
-
 parentContainer.appendChild(showHeading);
 showHeading.innerText = "Shows";
 
 //WRAP THE WHOLE SHOWS WITHOUT THE HEADING
-
 let parentContainerTwo = document.createElement("div");
 parentContainerTwo.classList.add("shows__container");
 parentContainer.appendChild(parentContainerTwo);
 
 let showTableWrapper = document.createElement("div");
 showTableWrapper.classList.add("shows__table");
-parentContainer.appendChild(showTableWrapper);
+parentContainerTwo.appendChild(showTableWrapper);
 
 //WRAPPER THE DATES VENUE LOCATION (TABLET AND DESKTOP VIEW)
 let rowWrapperHeader = document.createElement("div");
@@ -58,32 +22,27 @@ rowWrapperHeader.classList.add("shows__title--non-mobile");
 showTableWrapper.appendChild(rowWrapperHeader);
 
 let datesHeader = document.createElement("h5");
-datesHeader.classList.add("shows__header");
-datesHeader.classList.add("shows__title");
+datesHeader.classList.add("shows__header", "shows__title");
 //puts the class you made in the parent container
 rowWrapperHeader.appendChild(datesHeader);
 datesHeader.innerText = "DATES";
 
 let venueHeader = document.createElement("h5");
-venueHeader.classList.add("shows__header");
-venueHeader.classList.add("shows__title");
+venueHeader.classList.add("shows__header", "shows__title");
 rowWrapperHeader.appendChild(venueHeader);
 venueHeader.innerText = "VENUE";
 
 let locationHeader = document.createElement("h5");
-locationHeader.classList.add("shows__header");
-locationHeader.classList.add("shows__title");
+locationHeader.classList.add("shows__header", "shows__title");
 rowWrapperHeader.appendChild(locationHeader);
 locationHeader.innerText = "LOCATION";
 
 let transparentHeader = document.createElement("h5");
-transparentHeader.classList.add("shows__header");
-transparentHeader.classList.add("shows__title");
+transparentHeader.classList.add("shows__header", "shows__title");
 rowWrapperHeader.appendChild(transparentHeader);
 transparentHeader.innerText = "         ";
 
 //TABLET AND DESKTOP TABLE HEADERS END
-
 //WRAPPED MY ROWS SO THEY FLEX
 let showRowWrapper = document.createElement("div");
 parentContainerTwo.appendChild(showTableWrapper);
@@ -93,10 +52,9 @@ axios
     "https://project-1-api.herokuapp.com/showdates?api_key=5276e2d7-0178-438b-b62f-748cd9a1f823"
   )
   .then(response => {
-    console.log(response);
-    console.log(response.data[1].date);
+    let showsArr = response.data;
 
-    for (i = 0; i < response.data.length; i++) {
+    showsArr.forEach(showsData => {
       let rowWrapper = document.createElement("div");
       parentContainerTwo.appendChild(showTableWrapper);
       rowWrapper.classList.add("shows__wrapper");
@@ -109,16 +67,14 @@ axios
       headingDateMobile.innerText = "DATE";
       rowWrapper.appendChild(headingDateMobile);
 
-      //DATE FROM OBJECT showObj
+      //DATE FROM SHOWS API
       // //makes a variable and creats the element div inside the parent container
       let showsContainer = document.createElement("div");
       // //the variable shows container get than and put a class in there showDate
       showsContainer.classList.add("shows__text--bold"); //styling bold
       // //grab the wrapper and that's where you are going to put the new element you created
       rowWrapper.appendChild(showsContainer);
-      let x = (document.querySelectorAll(".shows__text--bold")[i].innerText =
-        response.data[i].date); //my prettier in vs code makes this multi-line
-      console.log(x); //checking if my for loop is working
+      showsContainer.innerText = showsData.date; //my prettier in vs code makes this multi-line
 
       //VENUE MOBILE HEADER
       let headingVenueMobile = document.createElement("div");
@@ -127,13 +83,11 @@ axios
       headingVenueMobile.innerText = "VENUE";
       rowWrapper.appendChild(headingVenueMobile);
 
-      //VENUE FROM OBJECT showObj
+      //VENUE FROM SHOWS API
       let showsContainerTwo = document.createElement("div");
       showsContainerTwo.classList.add("shows__text");
       rowWrapper.appendChild(showsContainerTwo);
-      let y = (document.querySelectorAll(".shows__text")[i].innerText =
-        response.data[i].place);
-      console.log(y);
+      showsContainerTwo.innerText = showsData.place;
 
       //LOCATION MOBILE HEADER
       let headingLocationMobile = document.createElement("div");
@@ -142,94 +96,20 @@ axios
       headingLocationMobile.innerText = "LOCATION";
       rowWrapper.appendChild(headingLocationMobile);
 
-      //LOCATION FROM OBJECT showObj
+      //LOCATION FROM SHOWS API
       let showsContainerThree = document.createElement("div");
       showsContainerThree.classList.add("shows__text-2");
-      showsContainerThree.classList.add("shows__text-2"); //add styling
       rowWrapper.appendChild(showsContainerThree);
-      let z = (document.querySelectorAll(".shows__text-2")[i].innerText =
-        response.data[i].location);
-      console.log(z);
+      showsContainerThree.innerText = showsData.location;
 
       //BUTTONS BUY TICKETS
       let buttonShowsDiv = document.createElement("div"); //container
-      let buttonShows = document.createElement("button");
       buttonShowsDiv.classList.add("shows__button");
+      rowWrapper.appendChild(buttonShowsDiv);
       document.querySelectorAll(".shows__button");
+      let buttonShows = document.createElement("button");
       buttonShows.classList.add("button");
-      document.querySelectorAll(".button");
       buttonShows.innerText = "BUY TICKETS";
       buttonShowsDiv.appendChild(buttonShows);
-      rowWrapper.appendChild(buttonShowsDiv);
-    }
+    });
   });
-
-//Method is for loop because when I did this on Wednesday and Thursday it was working. To practice my for loop coding
-// for (i = 0; i < showObj.length; i++) {
-//   let rowWrapper = document.createElement("div");
-//   parentContainerTwo.appendChild(showTableWrapper);
-//   rowWrapper.classList.add("shows__wrapper");
-//   showTableWrapper.appendChild(rowWrapper);
-
-//   //DATE MOBILE HEADER
-//   let headingDateMobile = document.createElement("div");
-//   headingDateMobile.classList.add("shows__title--mobile");
-//   document.querySelectorAll(".shows__title--mobile");
-//   headingDateMobile.innerText = "DATE";
-//   rowWrapper.appendChild(headingDateMobile);
-
-//   //DATE FROM OBJECT showObj
-//   // //makes a variable and creats the element div inside the parent container
-//   let showsContainer = document.createElement("div");
-//   // //the variable shows container get than and put a class in there showDate
-//   showsContainer.classList.add("shows__text--bold"); //styling bold
-//   // //grab the wrapper and that's where you are going to put the new element you created
-//   rowWrapper.appendChild(showsContainer);
-//   document.querySelectorAll(".shows__text--bold")[i].innerText =
-//     showObj[i].date;
-//   let x = (document.querySelectorAll(".shows__text--bold")[i].innerText =
-//     showObj[i].date); //my prettier in vs code makes this multi-line
-//   console.log(x); //checking if my for loop is working
-
-//   //VENUE MOBILE HEADER
-//   let headingVenueMobile = document.createElement("div");
-//   headingVenueMobile.classList.add("shows__title--mobile");
-//   document.querySelectorAll(".shows__title--mobile");
-//   headingVenueMobile.innerText = "VENUE";
-//   rowWrapper.appendChild(headingVenueMobile);
-
-//   //VENUE FROM OBJECT showObj
-//   let showsContainerTwo = document.createElement("div");
-//   showsContainerTwo.classList.add("shows__text");
-//   rowWrapper.appendChild(showsContainerTwo);
-//   let y = (document.querySelectorAll(".shows__text")[i].innerText =
-//     showObj[i].venue);
-//   console.log(y);
-
-//   //LOCATION MOBILE HEADER
-//   let headingLocationMobile = document.createElement("div");
-//   headingLocationMobile.classList.add("shows__title--mobile");
-//   document.querySelectorAll(".shows__title--mobile");
-//   headingLocationMobile.innerText = "LOCATION";
-//   rowWrapper.appendChild(headingLocationMobile);
-
-//   //LOCATION FROM OBJECT showObj
-//   let showsContainerThree = document.createElement("div");
-//   showsContainerThree.classList.add("shows__text-2");
-//   showsContainerThree.classList.add("shows__text-2"); //add styling
-//   rowWrapper.appendChild(showsContainerThree);
-//   let z = (document.querySelectorAll(".shows__text-2")[i].innerText =
-//     showObj[i].location);
-//   console.log(z);
-
-//   //BUTTONS BUY TICKETS
-//   let buttonShowsDiv = document.createElement("div"); //container
-//   let buttonShows = document.createElement("button");
-//   buttonShowsDiv.classList.add("shows__button");
-//   document.querySelectorAll(".shows__button");
-//   buttonShows.classList.add("button");
-//   document.querySelectorAll(".button");
-//   buttonShows.innerText = "BUY TICKETS";
-//   buttonShowsDiv.appendChild(buttonShows);
-//   rowWrapper.appendChild(buttonShowsDiv);
-// }
